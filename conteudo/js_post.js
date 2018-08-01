01a121510f2a7daf7a1e7dc00ee8f138617489ca
darkarmybrasil <41974886+darkarmybrasil@users.noreply.github.com>
2018-08-01 11:35:17 +0000

    Update js_post.js

--- conteudo/js_post.js
+++ conteudo/js_post.js
@@ -64,7 +64,8 @@ objeto.onload = function()
 //         }
 //     }
  document.getElementById('menu').innerHTML +=    '<li><a href="https://darkarmybrasil.github.io/conteudo/deep web/deep web.html"> Deep web</a></li><li><a href="https://darkarmybrasil.github.io/conteudo/hacking/hacking.html"> Hacking</a></li><li><a href="https://darkarmybrasil.github.io/conteudo/desafios/desafios.html"> Desafios</a></li><li><a href="https://darkarmybrasil.github.io/conteudo/redes/redes.html"> Redes</a></li>';
-    var nomes = document.getElementsByClassName('nome_site');
+ document.getElementById('nome').innerHTML = '<a href="https://darkarmybrasil.github.io">' + titulos.toString()+'</a>';
+ var nomes = document.getElementsByClassName('nome_site');
     for(var i =0; i < nomes.length; i++)
     {
         nomes[i].innerHTML = titulos.toString();    
